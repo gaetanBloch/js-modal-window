cac331d4d775a993ed07ed6cd71f917205529234
Gaëtan Bloch <gaetan.bloch@gmail.com>
2020-12-29 07:07:32 +0100

    Display modal on any button click

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const showModalButtons = document.querySelectorAll('.show-modal');
+const closeModalButton = document.querySelector('.close-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+showModalButtons.forEach(button =>
+  button.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  })
+);
